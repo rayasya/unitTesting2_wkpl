ebc6c91b83972dc4468fb04534a38fa6bd621b78
rayrayaray <rayasya.dziqi@gmail.com>
2023-11-03 14:15:39 +0700

    add comment detail information

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,13 @@
 public class Calculator {
+
+    // membuat function untuk penjumlahan dengan return berupa tipe data int
+    // memerlukan 2 parameter berupa int yang nantinya akan dijumlahkan
     public int add(int a, int b) {
         return a + b;
     }
 
+    // membuat function pengurangan dengan return berupa tipe data int
+    // memerlukan 2 parameter berupa int yang nantinya akan dikurangi
     public int substract(int a, int b) {
         return a - b;
     }
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,18 +1,47 @@
+
+//mengimport library yang akan digunakan untuk unit testing, yaitu assertEquals dan juga Test
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest {
+
+    // membuat unit testing yang ditandai dengan keyword @Test
+    // serta membuat function testAdd untuk dilakukan pengujian
     @Test
     public void testAdd() {
+        // instansiasi clas Calculator
         Calculator calculator = new Calculator();
+
+        /*
+         * memanggil function add pada class calculator dan mengisi parameternya, serta
+         * memasukkan hasilnya pada variable result dengan tipe data int
+         */
         int result = calculator.add(5, 5);
+
+        /*
+         * memastikan apakah value pada variable result sama dengan
+         * ekspektasi hasil pada fungsi assertEquals
+         */
         assertEquals(10, result);
     }
 
+    // membuat unit testing yang ditandai dengan keyword @Test
+    // serta membuat function testSubstract untuk dilakukan pengujian
     @Test
     public void testSubstract() {
+        // instansiasi clas Calculator
         Calculator calculator = new Calculator();
+
+        /*
+         * memanggil function substract pada class calculator dan mengisi parameternya,
+         * serta memasukkan hasilnya pada variable result dengan tipe data int
+         */
         int result = calculator.substract(10, 5);
+
+        /*
+         * memastikan apakah value pada variable result sama dengan
+         * ekspektasi hasil pada fungsi assertEquals
+         */
         assertEquals(5, result);
     }
 }
